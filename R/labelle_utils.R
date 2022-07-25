b399ddbe1830e5a374318ca7016ac596f954b690
Federico Marini <marinif@uni-mainz.de>
2022-07-25 17:57:24 +0200

    extending the prototype to link out to multiple resources, with example of having celltypist

--- R/labelle_utils.R
+++ R/labelle_utils.R
@@ -8,3 +8,25 @@ editor_to_vector_sanitized <- function(txt) {
   sub(" +$", "", rn)
 }
 
+
+celltype_2_html <- function(celltype,
+                            source = NULL) {
+  celltypist_button <- .link2celltypist(celltype)
+
+  mycontent <- paste0(
+    tags$b(celltype), tags$br(),
+    "Link to the celltypist: ", celltypist_button, tags$br(),
+    "Link to the GeneCards database: ", "some gene ", tags$br(),
+    tags$code("Something about the cell type")
+  )
+  return(HTML(mycontent))
+}
+
+.link2celltypist <- function(val) {
+  sprintf(
+    '<a href = "https://www.celltypist.org/encyclopedia/?celltype%s" target = "_blank" class = "btn btn-primary" style = "%s">%s</a>',
+    val,
+    .actionbutton_biocstyle,
+    val
+  )
+}
